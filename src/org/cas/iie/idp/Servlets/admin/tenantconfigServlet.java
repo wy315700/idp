d2cd54db853ff41d5ce904bb65a2028c46fba36b
wangyang <wy315700@gmail.com>
2013-05-05 20:53:01 +0800

    添加修改属性种类的功能
    添加修改属性种类的功能

--- src/org/cas/iie/idp/Servlets/admin/tenantconfigServlet.java
+++ src/org/cas/iie/idp/Servlets/admin/tenantconfigServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.cas.iie.idp.admin.groupAdmin;
 import org.cas.iie.idp.user.Configs;
 import org.cas.iie.idp.user.SamlConfigRole;
 import org.cas.iie.idp.user.TenantConfigRole;
@@ -31,8 +32,33 @@ public class tenantconfigServlet extends HttpServlet{
 					String returnjson = json.toJson(config.getAttributeset().get(attr));
 					response.getWriter().print(returnjson);
 				}
+			}else if(action.equals("delattr")){
+				String attrtype = request.getParameter("attrtype");
+				if(attrtype != null){
+					TenantConfigRole config = Configs.getthistenantconfig();
+					if(config.getAttributeset().containsKey(attrtype)){
+						groupAdmin groupadmin = new groupAdmin(attrtype);
+						if(groupadmin.getAllGroups(0, 0).size() != 0){
+							response.getWriter().print("false");
+						}else{
+							config.delAttribute(attrtype);
+							Configs.saveconfig(config);
+							response.getWriter().print("true");
+						}
+					}else{
+						response.getWriter().print("false");
+					}
+				}
+			}else if(action.equals("addattr")){
+				String attrtype = request.getParameter("attrtype");
+				String attrdispname = request.getParameter("attrdispname");
+				if(attrtype != null && attrdispname != null){
+					TenantConfigRole config = Configs.getthistenantconfig();
+					config.addAttribute(attrtype,attrdispname);
+					boolean resullt = Configs.saveconfig(config);
+					response.getWriter().print(resullt);
+				}
 			}
 		}
 	}
-
 }
